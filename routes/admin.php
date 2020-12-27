49a4d3da0c739167e35e1585782558814bf8a59a
gustavo <gustavodevaler@gmail.com>
2020-12-27 14:32:04 -0300

    Routes in separate file

---
+++ routes/admin.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+
+
+Route::get('/admin', [AdminController::class, 'index']);
+Route::get('/admin/{id}', [AdminController::class, 'show']);
+Route::get('/admin/{id}/create', [AdminController::class, 'create']);
+Route::post('/admin/store/{table}', [AdminController::class, 'store']);
+Route::post('/admin/{model}/{id}', [AdminController::class, 'destroy']);
--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,4 @@ Route::get('/', function () {
    return "hola";
 });
 
-Route::get('/admin', [AdminController::class, 'index']);
-Route::get('/admin/{id}', [AdminController::class, 'show']);
-Route::get('/admin/{id}/create', [AdminController::class, 'create']);
-Route::post('/admin/store/{table}', [AdminController::class, 'store']);
-Route::post('/admin/{model}/{id}', [AdminController::class, 'destroy']);
+
